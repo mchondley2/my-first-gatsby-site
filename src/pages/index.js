1a0cffaa43bfb54c41a72336e5955ba8dde68706
mchondley2 <mmar@glassbell.com>
2023-04-25 20:42:43 -0400

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage} from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -9,15 +10,16 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this amazing site by following the Gatsby Tutorial</p>
       <StaticImage
-        alt="placeholder"
+        alt="Cat"
         src="../images/cat-01.jpg"
       />
     </Layout>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+// Seo updates the browser tab to the page title, followed by the site name
+// for example: Home Page | My Amazing Site
+export const Head = () => <Seo title="Home Page" />
 
 // Step 3: Export your component
 export default IndexPage
